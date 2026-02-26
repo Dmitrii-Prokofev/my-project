e1cd6e29a75e999f6a112140121e5ec88a550eae
Dmitrii-Prokofev <prokofevdiman@gmail.com>
2026-02-26 18:49:18 +0300

    pdate  tests

--- tests/test_get_notes_api.py
+++ tests/test_get_notes_api.py
@@ -1,7 +1,2 @@
-import json
-
-
-def test_get_notes_initially_empty(api_request):
-    status, data = api_request("GET", "/notes")
-    assert status == 200
-    assert json.loads(data) == []
+def test_get_notes_initially_empty(get_notes):
+    assert get_notes() == []
